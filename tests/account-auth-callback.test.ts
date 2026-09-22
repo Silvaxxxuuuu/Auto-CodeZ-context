24c52240811d96453f880bfe28e68cc58f0b34b8
Gabriel Silva <sebastiao54e@gmail.com>
2026-09-21 19:29:15 -0500

    test(account): cover static hosted callback URI

--- tests/account-auth-callback.test.ts
+++ tests/account-auth-callback.test.ts
@@ -61,10 +61,9 @@ test('parseAccountAuthCallback accepts browser passkey callbacks', () => {
 
 test('parseAccountAuthCallback accepts hosted identity callbacks', () => {
   assert.deepEqual(
-    parseAccountAuthCallback('autocodez://auth/hosted?flowId=hosted-1&code=authorization-code&state=state-1'),
+    parseAccountAuthCallback('autocodez://auth/hosted?code=authorization-code&state=state-1'),
     {
       type: 'hosted',
-      flowId: 'hosted-1',
       code: 'authorization-code',
       state: 'state-1',
     },
